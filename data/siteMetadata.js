7142f182d186ca77ba053900a405cda703e19dff
Muhammad Arslan Aslam <hello@arslanaslam.me>
2022-05-09 01:36:01 +0500

    update Google Analytics G4 Property

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -18,9 +18,9 @@ const siteMetadata = {
   locale: 'en-US',
   analytics: {
     // supports plausible, simpleAnalytics or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    plausibleDataDomain: 'https://arslanaslam.me', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
-    googleAnalyticsId: 'UA-67990658-6', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: 'G-L2PFY01RMW', // e.g. UA-000000-2 or G-XXXXXXX
   },
   comment: {
     // Select a provider and use the environment variables associated to it
