c79a92e77889826c382404d9ac368556f254e173
Muhammad Arslan Aslam <hello@arslanaslam.me>
2022-08-22 06:00:01 +0500

    fonts update

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ posts }) {
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          <h1 className="text-3xl font-regular leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Thoughts are my own{' '}
             <i>
               <small>mostly!</small>
@@ -41,7 +41,7 @@ export default function Home({ posts }) {
                       <div className="space-y-6">
                         <div>
                           <time dateTime={date}>{formatDate(date)}</time>
-                          <h1 className="text-4xl font-bold leading-8 tracking-tight">
+                          <h1 className="text-4xl font-regular leading-8 tracking-tight">
                             <Link
                               href={`/blog/${slug}`}
                               className="text-gray-900 dark:text-gray-100"
